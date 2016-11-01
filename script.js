ef69d88fb83491a6219e94b5a2e616f42a27057d
Forrestbob <forrestbob2000@yahoo.com>
2016-11-01 00:52:55 -0400

    exercise1 finished

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#card-info").hide();
+	$("#wire-info").hide();
+	
+	$("input").click(function(){
+		console.log("buy!");
+		if($('#credit').is(':checked')){
+			$("#card-info").show();
+			$("#wire-info").hide();
+		}
+		else if($('#wire').is(':checked')){
+			$("#wire-info").show();
+			$("#card-info").hide();
+		}
+	});
+});
